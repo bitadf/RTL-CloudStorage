b78d87d573f650126ce729427c03881dd2503ed5
Bita <bita.dftaylor@gmail.com>
2025-03-24 17:53:15 +0330

    font changed

--- app/src/main/java/com/example/pcstorage/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/pcstorage/fragments/ProfileFragment.kt
@@ -1,60 +1,86 @@
 package com.example.pcstorage.fragments
 
 import android.os.Bundle
+import android.os.MemoryFile
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pcstorage.R
+import com.example.pcstorage.adapter.HomeGridAdapter
+import com.example.pcstorage.adapter.HomeRecyclerAdapter
+import com.example.pcstorage.data.HomeGridItem
+import com.example.pcstorage.data.RecentUploadsItem
+import com.example.pcstorage.databinding.FragmentProfileBinding
+import com.example.pcstorage.databinding.FragmentStorageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    private lateinit var binding: FragmentProfileBinding
+
+    private lateinit var folderAdapter : HomeGridAdapter
+    private lateinit var folderRecycler : RecyclerView
+
+    private lateinit var fileAdapter: HomeRecyclerAdapter
+    private lateinit var fileRecycler : RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater , container , false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bgColors = listOf(
+            ContextCompat.getColor(requireContext(),R.color.bridesmaid),
+            ContextCompat.getColor(requireContext(),R.color.solitude),
+            ContextCompat.getColor(requireContext(),R.color.early_down),
+            ContextCompat.getColor(requireContext(),R.color.twilight_blue)
+
+        )
+
+        val colors = listOf(
+            ContextCompat.getColor(requireContext(),R.color.carnation),
+            ContextCompat.getColor(requireContext(),R.color.cornflower_blue),
+            ContextCompat.getColor(requireContext(),R.color.saffron),
+            ContextCompat.getColor(requireContext(),R.color.turquoise_blue)
+
+        )
+        val folderTitles = listOf(
+            "فولدر یک" ,
+            "فولدر دو"
+        )
+        folderRecycler = binding.profileSharedFolderRecycler
+        folderRecycler.layoutManager = GridLayoutManager(requireContext() , 2)
+        val folderItem = mutableListOf<HomeGridItem>()
+        for(i in folderTitles.indices){
+            folderItem.add(HomeGridItem(folderTitles[i] , "230Gb" , bgColors[i] , colors[i]))
+        }
+        folderAdapter = HomeGridAdapter(folderItem)
+        folderRecycler.adapter = folderAdapter
+
+        val sharedFileItem = listOf(
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+            RecentUploadsItem("dick.txt" , "22/22/22" , "250Mb" , 1) ,
+        )
+        fileRecycler = binding.profileSharedFilesRecycler
+        fileRecycler.layoutManager = LinearLayoutManager(requireContext())
+        fileAdapter = HomeRecyclerAdapter(sharedFileItem)
+        fileRecycler.adapter = fileAdapter
     }
+
+
 }
